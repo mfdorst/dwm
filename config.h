a5c9ff7f8e7e3735e4f7f0fddf5bb4897646b3a1
Michael Dorst <m@mdorst.net>
2021-04-23 19:41:25 -0700

    Reset keybinding for new terminal to Mod+Shift+Return

--- config.h
+++ config.h
@@ -69,7 +69,7 @@ static const char *downvol[] = { "/usr/bin/amixer", "-c", "1", "set", "Master", 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
